8ce8f357a53617073a1f6f5a94cad3cb4aa17e4e
Shengliang Cai <cubercsl@163.com>
2020-11-07 22:25:32 +0800

    feat: latexindent

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ set -e
 cd $(dirname $0)
 for subdir in `find . -regex '\./[0-9]*'`; do
     cd $subdir
-    latexmk -xelatex -shell-escape
+    latexindent  -y="defaultIndent: '    '" -w *.tex
+    latexmk -xelatex -shell-escape --outdir=output
+    rm -rf output/
     cd -
 done
