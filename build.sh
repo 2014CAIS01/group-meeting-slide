a1f58aa2d56f6c14eb5d5f8bf2cfe83e077a4bcd
Shengliang Cai <cubercsl@163.com>
2020-11-07 23:26:55 +0800

    fix: CI

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 cd $(dirname $0)
 for subdir in `find . -regex '\./[0-9]*'`; do
     cd $subdir
-    latexindent  -y="defaultIndent: '    '" -w *.tex
+    latexindent -s -y="defaultIndent: '    '" -w *.tex
     latexmk -xelatex -shell-escape --outdir=output
     rm -rf output/
     cd -
