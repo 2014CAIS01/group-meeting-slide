944e8f46935de02877cb89404e81689e8fee30a7
Shengliang Cai <cubercsl@163.com>
2020-11-08 13:33:48 +0800

    fix: CI

--- build.sh
+++ build.sh
@@ -3,8 +3,23 @@ set -e
 cd $(dirname $0)
 for subdir in `find . -regex '\./[0-9]*'`; do
     cd $subdir
-    latexindent -s -y="defaultIndent: '    '" -w *.tex
+
+    # Indent
+    echo '##########' Indent $subdir '##########'
+    for file in \
+        `find . -name "*.tex" \
+                -o -name "*.sty" \
+                -o -name "*.cls" \
+                -o -name "*.bib"`; do
+        latexindent -y="defaultIndent: '    '" -w -sl -s $file
+    done
+
+    # Build
+    echo '##########' Build $subdir '##########'
     latexmk -xelatex -shell-escape --outdir=output
+
+    # Clean
     rm -rf output/
+    rm *.bak* indent.log
     cd -
 done
